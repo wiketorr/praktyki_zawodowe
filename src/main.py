17ff0578cf9d74629a626364efd1a1dc7dff5a89
wiketorr <wiktorfluder2008@interia.pl>
2025-10-15 12:28:02 +0000

    changed variable names to English

--- src/main.py
+++ src/main.py
@@ -7,16 +7,16 @@ app = FastAPI(title="RPG sim")
 
 def get_number(dice:int) -> int:
     response =  requests.get(f"https://www.random.org/integers/?num=1&min=1&max={dice}&col=1&base=10&format=plain&rnd=new")
-    wynik = int(response.text.strip())
-    return wynik
+    result = int(response.text.strip())
+    return result
 
 def get_rolls(dice:int, rolls:int) -> list[int]:
     return [get_number(dice) for _ in range(rolls)]
 
 @app.get("/roll")
 def response_dice_rolls(dice: int, rolls: int) -> dict[str,list[int]]:
-    wynik = get_rolls(dice, rolls)
-    return {"wynik": wynik}
+    result = get_rolls(dice, rolls)
+    return {"result": result}
 
 
 def roll_dice(dice:int) -> int:
@@ -28,5 +28,5 @@ def get_rolls_secrets(dice:int, rolls:int) -> list[int]:
 @app.get("/secret_roll")
 def response_secret(dice: int, rolls: int) -> dict[str,list[int]]:
     result = get_rolls_secrets(dice, rolls)
-    return {"wynik": result}
+    return {"result": result}
 
